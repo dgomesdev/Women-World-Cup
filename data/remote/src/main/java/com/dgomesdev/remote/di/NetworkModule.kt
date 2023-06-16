eb353047e5ef5ad9b5c90ae3ededdbde7d452c9b
dgomesdev <danilo.gomes@outlook.fr>
2023-06-15 23:03:45 -0300

    JSON modified

--- data/remote/src/main/java/com/dgomesdev/remote/di/NetworkModule.kt
+++ data/remote/src/main/java/com/dgomesdev/remote/di/NetworkModule.kt
@@ -16,7 +16,7 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
-    private val url = "https://dgomesdev.github.io/Women-World-Cup/docs/JSONapi/"
+    private val url = "https://dgomesdev.github.io/Api-simulations/"
 
     @Provides
     @Singleton
--- data/remote/src/main/java/com/dgomesdev/remote/services/MatchesServices.kt
+++ data/remote/src/main/java/com/dgomesdev/remote/services/MatchesServices.kt
@@ -5,6 +5,6 @@ import retrofit2.http.GET
 
 interface MatchesServices {
 
-    @GET("MatchesAPI.json")
+    @GET("womenWorldCup.json")
     suspend fun getMatchesService(): List<MatchRemote>
 }
